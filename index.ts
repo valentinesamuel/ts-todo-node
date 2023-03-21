e3ca057b45cdd6cbaf011ad1b9d377e364fedb22
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-21 16:40:42 +0000

    FEAT: Connected to localhost mysql

--- index.ts
+++ index.ts
@@ -4,10 +4,16 @@ import express, {
   Request,
 } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'
 import { DataSource } from 'typeorm';
+import bodyParser from 'body-parser';
 
 const app: Express = express();
 dotenv.config();
+
+app.use(bodyParser.json())
+
+app.use(cors())
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
@@ -25,10 +31,14 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + Typscript');
 });
 
-AppDataSource.initialize().then(() => {
-  app.listen(port);
-  console.log('🚀 Data Source has been initialized');
-}).catch((err) => {
-  console.error('Error during Data Source initialization', err);
-  
-})
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port);
+    console.log('🚀 Data Source has been initialized');
+  })
+  .catch((err) => {
+    console.error(
+      'Error during Data Source initialization',
+      err,
+    );
+  });
